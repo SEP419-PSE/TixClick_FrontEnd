b5fa88777dbb3a0a4fd4b6b7a93084b25a9005f7
LuanVM1810 <Vomongluan@gmail.com>
2025-01-12 19:17:02 +0700

    update footer and responsive

--- src/components/HeroSlider/HeroSlider.tsx
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -1,5 +1,15 @@
 const HeroSlider = () => {
-  return <div>HeroSlider</div>;
+  return (
+    <div className="mx-4 my-8">
+      <div className="relative w-full lg:w-[30%] h-60 overflow-hidden rounded-lg">
+        <img
+          src="https://images.tkbcdn.com/2/614/350/ts/ds/f0/ca/7c/1c7973947f9b163b2376dc6bdc0c6540.jpg"
+          alt="Thumbnail"
+          className="absolute inset-0 w-full h-full object-cover transition-opacity duration-300 opacity-100 group-hover:opacity-0"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HeroSlider;
