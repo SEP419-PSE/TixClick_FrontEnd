201d998a8ffe85e4496b15056b997a4fe18c1e77
Huy-techSw <anchorle3543@gmail.com>
2025-03-02 00:27:15 +0700

    Updated company api

--- src/interface/manager/Company.ts
+++ src/interface/manager/Company.ts
@@ -1,13 +1,26 @@
+export interface Account {
+  accountId: number;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  phone: string | null;
+  active: boolean;
+  avatarURL: string | null;
+  dob: string | null;
+  roleId: number;
+}
+
 export interface Company {
-    companyId: number;
-    companyName: string;
-    codeTax: string;
-    bankingName: string;
-    bankingCode: string;
-    nationalId: string;
-    logoURL: string;
-    description: string;
-    status: "PENDING" | "ACTIVE" | "INACTIVE"; 
-    representativeId: number;
-  }
-  
+  companyId: number;
+  companyName: string;
+  codeTax: string;
+  bankingName: string;
+  bankingCode: string;
+  nationalId: string;
+  logoURL: string;
+  description: string;
+  status: "PENDING" | "ACTIVE" | "INACTIVE";
+  representativeId: number;
+  accountDTO: Account;
+}
