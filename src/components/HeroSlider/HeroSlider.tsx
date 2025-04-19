abb0bc392763e63d802876fd4ee2eb0819166d50
LuanVM1810 <Vomongluan@gmail.com>
2025-04-19 14:57:50 +0700

    update hero slider

--- src/components/HeroSlider/HeroSlider.tsx
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -78,9 +78,6 @@ const HeroSlider = () => {
   return (
     <div className="">
       <div className="relative">
-        {/* <img src="https://images.tkbcdn.com/2/614/350/ts/ds/2b/cf/04/28f7e94ace8d993b981003b88b26b0b5.jpg" />
-        <img src="https://images.tkbcdn.com/2/614/350/ts/ds/2a/83/ac/cb6963360dae30f95f517cefc982780e.jpg" />
-        <img src="https://images.tkbcdn.com/2/614/350/ts/ds/8a/02/c3/c9d60e7974b86bb46ff88e96ae91b3cc.jpg" /> */}
         <video
           autoPlay
           loop
