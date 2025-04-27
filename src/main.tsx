975ce1ec3f83811415260f991fdd7674f4369001
caleb1123 <ryy1507@gmail.com>
2025-04-27 17:35:09 +0700

    update payment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // 🛠️ BẠN ĐANG THIẾU DÒNG NÀY
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/AuthProvider.tsx";
 import { LanguageProvider } from "./pages/organizer/components/LanguageContext.tsx";
@@ -8,6 +9,7 @@ import { SidebarProvider } from "./contexts/SideBarContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <BrowserRouter>
     <AuthProvider>
       <LanguageProvider>
         <SidebarProvider>
@@ -15,5 +17,6 @@ createRoot(document.getElementById("root")!).render(
         </SidebarProvider>
       </LanguageProvider>
     </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
